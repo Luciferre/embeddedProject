9d50a08e3a8d770627c8f5356d2a48eed86e7126
jason <jason@jason-VirtualBox.(none)>
2014-11-02 13:12:04 -0500

    enable IRQ in cpsr

--- lab3-support/kernel/irqHandler_c.c
+++ lab3-support/kernel/irqHandler_c.c
@@ -1,11 +1,19 @@
 #include <types.h>
 #include <arm/reg.h>
 #include <arm/timer.h>
+#include <exports.h>
+#include <arm/interrupt.h>
 
 volatile unsigned int system_time;
 volatile unsigned int time_drift;
 void irqHandler(){
-    unsigned int oscrValue;
+    unsigned volatile int oscrValue, icprValue;
+    
+    icprValue = reg_read(INT_ICPR_ADDR);
+    
+    printf("irq %d\n",icprValue);
+    //if((icprValue & (1<<INT_OSTMR_0)) == 0)
+     //   return;
     system_time++;
 
     oscrValue = reg_read(OSTMR_OSCR_ADDR);
@@ -16,7 +24,7 @@ void irqHandler(){
         system_time++;
         time_drift -= OSTMR_COUNT;
     }
-
+    reg_write(OSTMR_OSCR_ADDR,0); 
     /*achknowledge the IRQ and clear it*/
     reg_set(OSTMR_OSSR_ADDR, OSTMR_OSSR_M0);
 
--- lab3-support/kernel/timerIrqSetup.c
+++ lab3-support/kernel/timerIrqSetup.c
@@ -13,11 +13,13 @@ void timerIrqSetup(){
     /*clear the OSTMR interrupt bit in the ICLR to generates IRQ*/
     reg_clear(INT_ICLR_ADDR, 1<<INT_OSTMR_0);
     /*enable the os timer interrupt*/
-    reg_set(INT_ICMR_ADDR, 1<<INT_OSTMR_0);
+    reg_write(INT_ICMR_ADDR, 1<<INT_OSTMR_0);
     /*clear the OSCR*/
-    reg_clear(OSTMR_OSCR_ADDR, 0xffffffff);
+    //reg_clear(OSTMR_OSCR_ADDR, 0xffffffff);
     /*set the value of OSMR 0 to 10ms*/
-    reg_write(OSTMR_OSMR_ADDR(0),OSTMR_COUNT);
+    //reg_write(OSTMR_OSMR_ADDR(0),OSTMR_COUNT);
+    reg_write(OSTMR_OSMR_ADDR(0),32500000);
     /*enable the OSMR0 */
     reg_set(OSTMR_OIER_ADDR, OSTMR_OIER_E0);
+    reg_clear(OSTMR_OSCR_ADDR, 0xffffffff);
 }
